0c2494bf3637de51969552cb07d6fa4f352592b1
Kamil Koczkowski <kamilk@kainos.com>
2018-12-06 23:53:07 +0000

    added exercise, not yet complete

---
+++ sda_demo/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        String[] tablicaImion = new String[]{"Marek", "Zdenek", "Zdzichu", "Kazik", "Rysio", "Ala", "Roksana", "Monika", "Dobrawa", "Wladyslawa"};
+        int[] tablicaWiek = new int[]{23, 34, 33, 28, 22, 15, 17, 29, 39, 55};
+        String[] tablicaSex = new String[]{"M", "M", "M", "M", "M", "W", "W", "W", "W", "W"};
+
+        for (int i = 0; i < tablicaImion.length; i++) {
+            Osoba osoba = new Osoba();
+            osoba.setImie(tablicaImion[i]);
+            int rand = (int) (Math.random() * tablicaImion.length);
+
+            osoba.setRokUrodzenia(tablicaWiek[rand]);
+            osoba.setSex(tablicaSex[rand]);
+            osoba.przedstawSie();
+        }
+    }
+}
---
+++ sda_demo/src/Osoba.java
@@ -0,0 +1,39 @@
+public class Osoba {
+
+    private String imie;
+    private int rokUrodzenia;
+    private String sex;
+
+    public Osoba() {
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public int getRokUrodzenia() {
+        return rokUrodzenia;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setRokUrodzenia(int rokUrodzenia) {
+        this.rokUrodzenia = rokUrodzenia;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void przedstawSie() {
+
+        System.out.println("mam na imie " + imie + " ,rok urodzenia " + rokUrodzenia + " moja plec to " + sex + "");
+    }
+
+}
