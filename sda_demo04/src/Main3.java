8b6fe9894df47fce380e0ec81ee9e4330148191a
Kamil Koczkowski <kamilk@kainos.com>
2019-01-07 16:04:58 +0100

    added Student class

--- sda_demo04/src/Main3.java
+++ sda_demo04/src/Main3.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main3 {
 
     public static void main(String[] args) {
@@ -7,6 +10,15 @@ public class Main3 {
 
         System.out.println("moj pierwszy komp ma "+pierwszyKomp.getTyp().iloscRdzeni()+"");
         System.out.println("moj drugi komp ma "+drugiKomp.getTyp().iloscRdzeni()+"");
+
+        List<Double> listaJakichsOcen = new ArrayList<>();
+        listaJakichsOcen.add(4.5);
+        listaJakichsOcen.add(5.0);
+        listaJakichsOcen.add(3.5);
+
+        Student s1 = new Student(listaJakichsOcen,12791,"Kamil","Koczkowski");
+
+        System.out.println(s1);
 
     }
 
